160803419cf6d1a63c948bec28ca4293ae12b8ff
amykangweb <selene6023@gmail.com>
2015-04-04 02:08:17 -0700

    Update spec and todo_list.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'rspec/autorun'
 require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -17,8 +18,18 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+RSpec.configure do |c|
+  c.expose_current_running_example_as :example
+end
+
 RSpec.configure do |config|
-  # ## Mock Framework
+  # Turn off deprecation warnings
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
   #
